8014b7a3ee0e23aedccb45bdfc28aeae389ac1f7
tangk <tangk@yikga.com>
2020-06-17 21:35:56 +0800

    首次提交

---
+++ src/main/java/com/tk/code/bean/Employee.java
@@ -0,0 +1,10 @@
+package com.tk.code.bean;
+
+/**
+ * @program: MyBatis
+ * @description:
+ * @author: tk
+ * @create: 2020-06-17 21:34
+ **/
+public class Employee {
+}
---
+++ src/main/java/com/tk/code/dao/EmployeeDao.java
@@ -0,0 +1,10 @@
+package com.tk.code.dao;
+
+/**
+ * @program: MyBatis
+ * @description:
+ * @author: tk
+ * @create: 2020-06-17 21:34
+ **/
+public class EmployeeDao {
+}
